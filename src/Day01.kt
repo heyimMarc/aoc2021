53490b82c4eddf4bbbaee7209d3f0581d9e99229
Marc Nützel <me@marc-nuetzel.de>
2021-12-02 16:39:56 +0100

    solves Day1 🦄

--- src/Day01.kt
+++ src/Day01.kt
@@ -3,18 +3,27 @@ import java.util.concurrent.atomic.AtomicInteger
 private const val DAY_01_TXT = "Day01"
 
 fun main() {
-    val input = readInput(DAY_01_TXT)
+    val input = readInput(DAY_01_TXT).map { it.toInt() }
+    println("Value for part one: " + partOne(input))
+    println("Value for part two: " + partTwo(input))
+}
+
+fun partTwo(input: List<Int>): Int {
+    val windowed = input.windowed(3).map { it.sum() }
+    return partOne(windowed)
+}
+
+fun partOne(input: List<Int>): Int {
     val amountOfIncreased = AtomicInteger(0)
     var prevVal: Int? = null
 
     input.forEach { line ->
-        if (prevVal != null && isIncreased(prevVal!!, line.toInt())) {
+        if (prevVal != null && isIncreased(prevVal!!, line)) {
             amountOfIncreased.incrementAndGet()
         }
-        prevVal = line.toInt()
-
+        prevVal = line
     }
-    println(amountOfIncreased)
+    return amountOfIncreased.get()
 }
 
 fun isIncreased(prev: Int, current: Int): Boolean {
